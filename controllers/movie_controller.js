7921f13ce6f313da07b72930209d3ba54d7b7d00
Lei <leichen83@gmail.com>
2019-02-23 12:40:47 -0500

    t2

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -1,162 +1,153 @@
 // Import NPM dependency and Express router function.
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var request = require('request');
+var request = require("request");
 
 // Import data model.
-var db = require('../models');
+var db = require("../models");
 
 // GET route which calls uses Sequelize's findAll method.
 // This route then hands the data it receives to handlebars so index can be rendered.
-router.get('/', function(req, res) {
-    db.Movie.findAll({
-        order: 'movie_name ASC'
-
-    }).then(function(data) {
-        var hbsObject = {
-            movies: data
-        };
-        res.render('index', hbsObject);
-    });
+router.get("/", function(req, res) {
+  db.Movie.findAll({
+    order: "movie_name ASC"
+  }).then(function(data) {
+    var hbsObject = {
+      movies: data
+    };
+    res.render("index", hbsObject);
+  });
 });
 
-router.get('/year', function(req, res) {
-    db.Movie.findAll({
-        order: 'movie_year DESC'
-
-    }).then(function(data) {
-        var hbsObject = {
-            movies: data
-        };
-        res.render('index', hbsObject);
-    });
+router.get("/year", function(req, res) {
+  db.Movie.findAll({
+    order: "movie_year DESC"
+  }).then(function(data) {
+    var hbsObject = {
+      movies: data
+    };
+    res.render("index", hbsObject);
+  });
 });
 
-router.get('/rating', function(req, res) {
-    db.Movie.findAll({
-        order: 'movie_ratingImdb DESC'
-
-    }).then(function(data) {
-        var hbsObject = {
-            movies: data
-        };
-        res.render('index', hbsObject);
-    });
+router.get("/rating", function(req, res) {
+  db.Movie.findAll({
+    order: "movie_ratingImdb DESC"
+  }).then(function(data) {
+    var hbsObject = {
+      movies: data
+    };
+    res.render("index", hbsObject);
+  });
 });
-
 
 // POST route which calls Sequelize's create method with the movie name given.
-router.post('/api/new/movie', function(req, res) {
+router.post("/api/new/movie", function(req, res) {
+  var movieName = req.body.name;
 
-    var movieName = req.body.name;
+  var queryUrl =
+    "http://www.omdbapi.com/?i=tt3896198&apikey=9613e41&t=" + movieName;
 
-    var queryUrl = "http://omdbapi.com/?apikey=40e9cece&t=" + movieName;
+  request(queryUrl, function(error, response, body) {
+    if (!error && JSON.parse(body).Response !== "False") {
+      console.log(JSON.parse(body));
 
-    request(queryUrl, function(error, response, body) {
+      var imdbId = JSON.parse(body).imdbID;
 
+      console.log(imdbId);
 
-        if (!error && JSON.parse(body).Response !== 'False') {
-            console.log(JSON.parse(body));
+      var videos = "";
 
-            var imdbId = JSON.parse(body).imdbID;
+      var options = {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/movie/" + imdbId + "/videos",
+        qs: {
+          language: "en-US",
+          api_key: "5e954f184aa02d384bc22bbc96df2ffa"
+        },
+        body: "{}"
+      };
 
-            console.log(imdbId);
+      request(options, function(error, response, result) {
+        if (error) res.redirect("/");
 
-            var videos = "";
-
-            var options = {
-                method: 'GET',
-                url: 'https://api.themoviedb.org/3/movie/' + imdbId + '/videos',
-                qs: {
-                    language: 'en-US',
-                    api_key: 'd50548305ff81a83c1c65efa4ce59583'
-                },
-                body: '{}'
-            };
-
-            request(options, function(error, response, result) {
-
-                if (error) res.redirect('/');
-
-
-                // if (error) {
-                //     alert("Seems to be a problem with your input. Please try again");
-                //     //res.redirect('/');
-                // } else {
-                if (!JSON.parse(result).results) {
-                    // res.send('SOMETHING WENT WRONG');
-                    res.redirect('/')
-                } else {
-                    videos = JSON.parse(result).results[0].key;
-                    console.log(videos);
-                    db.Movie.create({
-                        movie_name: JSON.parse(body).Title,
-                        movie_poster: JSON.parse(body).Poster,
-                        movie_genre: JSON.parse(body).Genre,
-                        movie_time: JSON.parse(body).Runtime,
-                        movie_plot: JSON.parse(body).Plot,
-                        movie_director: JSON.parse(body).Director,
-                        movie_actors: JSON.parse(body).Actors,
-                        movie_year: JSON.parse(body).Year,
-                        movie_trailer: videos,
-                        movie_ratingImdb: JSON.parse(body).Ratings[0].Value,
-                        movie_ratingRotten: JSON.parse(body).Ratings[1].Value
-
-                    }).then(function() {
-                        res.redirect('/');
-                    });
-
-                }
-            });
-
+        // if (error) {
+        //     alert("Seems to be a problem with your input. Please try again");
+        //     //res.redirect('/');
+        // } else {
+        if (!JSON.parse(result).results) {
+          // res.send('SOMETHING WENT WRONG');
+          res.redirect("/");
         } else {
-            console.log("\nOops...something went wrong with you movie search. Try again...");
-            res.redirect('/');
+          videos = JSON.parse(result).results[0].key;
+          console.log(videos);
+          db.Movie.create({
+            movie_name: JSON.parse(body).Title,
+            movie_poster: JSON.parse(body).Poster,
+            movie_genre: JSON.parse(body).Genre,
+            movie_time: JSON.parse(body).Runtime,
+            movie_plot: JSON.parse(body).Plot,
+            movie_director: JSON.parse(body).Director,
+            movie_actors: JSON.parse(body).Actors,
+            movie_year: JSON.parse(body).Year,
+            movie_trailer: videos,
+            movie_ratingImdb: JSON.parse(body).Ratings[0].Value,
+            movie_ratingRotten: JSON.parse(body).Ratings[1].Value
+          }).then(function() {
+            res.redirect("/");
+          });
         }
-    });
+      });
+    } else {
+      console.log(
+        "\nOops...something went wrong with you movie search. Try again..."
+      );
+      res.redirect("/");
+    }
+  });
 });
-
 
 // update method to mark that movie as watched.
-router.put('/api/new/watched/:id', function(req, res) {
+router.put("/api/new/watched/:id", function(req, res) {
+  var watched = true;
+  var ID = req.params.id;
 
-    var watched = true;
-    var ID = req.params.id;
-
-    db.Movie.update({
-        watched: watched,
-
-    }, { where: { id: ID } }).then(function() {
-        res.redirect('/');
-    });
+  db.Movie.update(
+    {
+      watched: watched
+    },
+    { where: { id: ID } }
+  ).then(function() {
+    res.redirect("/");
+  });
 });
 
-
 // PUT (update) route which calls Sequelize's update method to mark the movie as not yet watched .
-// Sends the id to identify which movie. 
-router.put('/:id', function(req, res) {
-    var watched = false;
-    var ID = req.params.id;
+// Sends the id to identify which movie.
+router.put("/:id", function(req, res) {
+  var watched = false;
+  var ID = req.params.id;
 
-    db.Movie.update({
-        watched: watched,
-
-    }, { where: { id: ID } }).then(function() {
-        res.redirect('/');
-    });
+  db.Movie.update(
+    {
+      watched: watched
+    },
+    { where: { id: ID } }
+  ).then(function() {
+    res.redirect("/");
+  });
 });
 
 // Deleting a movie
 
-router.delete('/api/new/delete/:id', function(req, res) {
+router.delete("/api/new/delete/:id", function(req, res) {
+  var ID = req.params.id;
 
-    var ID = req.params.id;
-
-    db.Movie.destroy({
-        where: { id: ID }
-    }).then(function() {
-        res.redirect('/');
-    });
+  db.Movie.destroy({
+    where: { id: ID }
+  }).then(function() {
+    res.redirect("/");
+  });
 });
 
 // Export routes for server.js.
